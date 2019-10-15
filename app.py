9e08cb2ed2a50ce2f918ee2b9b71e18a510cc20d
= <zainr7989@gmail.com>
2019-10-15 16:25:48 -0700

    Update route to store file upload in database.

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
-from flask import Flask, render_template
-
+from flask import Flask, flash, render_template, request, redirect, url_for
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+
+# Tell Flask how to find our database
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Compliance')
+client = MongoClient()
+db = client.Compliance
+documents = db.documents
 
 
 @app.route('/')
 def index():
-    """Work on file input page"""
-    return render_template("home.html")
+    """User uploads file here."""
+    return render_template("submission_new.html")
 
 
-@app.route('/submission/form_tracker', methods=["POST"])
-def form_tracker():
-    """Track the forms which have been submitted so far."""
-    return "Hi."
+@app.route('/submission/form_tracker/upload', methods=["POST"])
+def form_upload():
+    """Add the uploaded file to the database."""
+
+    if 'userFile' in request.files:
+        # Make a new document JSON from form data
+        file = request.files['userFile']
+        file.save(file)
+
+        new_doc = {
+            "file_name": file.filename
+        }
+
+    # insert into PyMongo database
+    documents.insert_one(new_doc)
+
+    # return name of file to check it was uploaded ok
+    return file.filename
 
 
 if __name__ == '__main__':

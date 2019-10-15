500258483cb1c9659ded21e18bc6ece0804b5133
= <zainr7989@gmail.com>
2019-10-15 13:58:04 -0700

    [ADD] File input form.

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    """Display welcome message."""
-    return "Hello World! What are you working on today?"
+    """Work on file input page"""
+    return render_template("home.html")
 
 
 if __name__ == '__main__':

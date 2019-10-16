b673d2ea8f11185d273fe7609a78f56fb0c61e44
= <zainr7989@gmail.com>
2019-10-15 18:28:55 -0700

    [FIX] Simplify routes to not include zip folder creation yet.

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 from werkzeug.utils import secure_filename
 import os
+import zipfile
+# from document import FormDocument
 
 app = Flask(__name__)
 
@@ -12,18 +14,54 @@ client = MongoClient()
 db = client.Compliance
 documents = db.documents
 
+# MOCK array of requirements to fill out, and image links to represent each
+links = [
+    "https://accidentattorneys.org/wp-content/uploads/2015/03/13080769_l.jpg",
+    "https://www.myairbags.com/wp-content/uploads/2018/07/red.jpg",
+    "http://img1.photo138.com/MS1/ZJ6396700-C-4-9.jpg",
+    "https://cdn-images-1.medium.com/max/1200/1*Ef6QU6hyuyvvqVfl87a1qw.png"
+]
+requirements = [
+    {"name": "Regulation A",
+     "description": "You must make sure the airbags are working.",
+     "image": links[0]},
+    {"name": "Regulation B",
+     "description": "You better have seatbelts.",
+     "image": links[1]},
+    {"name": "Regulation C",
+     "description": "Don't forget forget about my cupholders!",
+     "image": links[2]},
+    {"name": "Regulation D",
+     "description": "Make sure that self-driving algorithm values human life!",
+     "image": links[3]}
+]
+
 
 @app.route('/')
-def index():
+def show_requirements():
+    """Show user all the requireements that need to be completed."""
+    return render_template("requirements_index.html")
+
+
+@app.route('/submissions/form_tracker')
+def form_upload():
     """User uploads file here."""
     return render_template("submission_new.html")
 
 
 @app.route('/submission/form_tracker/upload', methods=["POST"])
-def form_upload():
+def form_new():
     """Add the uploaded file to the database."""
 
     if 'userFile' in request.files:
+        '''
+        file = request.files['userFile']
+        file_name = secure_filename(file.filename)
+        doc = FormDocument()
+        doc.filename = file_name
+        return "This works so far"
+
+        '''
         # Make a new document JSON from form data
         file = request.files['userFile']
         file.save(file)
@@ -32,17 +70,18 @@ def form_upload():
             "file_name": file.filename
         }
 
-    # insert into PyMongo database
-    documents.insert_one(new_doc)
+        # insert into PyMongo database
+        documents.insert_one(new_doc)
 
-    # return name of file to check it was uploaded ok
-    return redirect(url_for('index'))
+        # return name of file to check it was uploaded ok
+        return redirect(url_for('form_upload'))
 
 
-@app.route('/submission/download_zip', methods=["GET"])
+@app.route('/submission/download_zip')
 def get_zip():
-    """Takes all files in database and return a downloadable ZIP folder."""
-    return "This is where your ZIP folder will be."
+    """Takes all files in database and return a downloadable ZIP folder.
+    V1: this is first iteration, just serving file back to user using POST"""
+    return render_template("submission_complete.html")
 
 
 if __name__ == '__main__':

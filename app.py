3bb90926f80c51292760e102a81f9405cc4391a5
= <zainr7989@gmail.com>
2019-10-18 10:55:55 -0700

    [FIX] Test adding extra insert/remove statements.

--- app.py
+++ app.py
@@ -48,9 +48,12 @@ list_of_requirements = [
      "num_submitted": 0}
 ]
 # remove unneccesary requirements
-requirements.remove()
+requirements.delete_many({})
 # adds requirements to DB
 requirements.insert_many(list_of_requirements)
+
+# remove unneccesary requirements
+requirements.delete_many({})
 
 
 @app.route('/')

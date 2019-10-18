0049971fb80737c521f79a22ba0abc5e7b562d16
= <zainr7989@gmail.com>
2019-10-18 14:32:26 -0700

    [ADD] Requirements to database after removing extraneous ones.

--- app.py
+++ app.py
@@ -47,9 +47,12 @@ list_of_requirements = [
      "num_submitted": 0}
 ]
 
-documents.delete_many({})
 requirements.delete_many({})
-requirements.insert(list_of_requirements)
+requirements.insert_one(list_of_requirements[0])
+requirements.insert_one(list_of_requirements[1])
+requirements.insert_one(list_of_requirements[2])
+requirements.insert_one(list_of_requirements[3])
+
 
 @app.route('/')
 def show_requirements():

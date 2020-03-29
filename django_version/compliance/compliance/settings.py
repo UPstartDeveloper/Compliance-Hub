bfbdb44addc5caf6c481dd88d2fd9add8ef4cee0
= <zainr7989@gmail.com>
2020-03-29 10:46:04 -0400

    Update urls for landing page images.

--- django_version/compliance/compliance/settings.py
+++ django_version/compliance/compliance/settings.py
@@ -132,7 +132,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILE_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # provision PostgreSQL for deployment
 db_from_env = dj_database_url.config()
